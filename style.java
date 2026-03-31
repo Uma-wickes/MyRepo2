a993cbfbaae86eead6cb683effedb2469d58e441
Uma-wickes <umachaganti99@gmail.com>
2026-03-31 13:15:32 +0100

    data added on style.java

---
+++ style.java
@@ -0,0 +1,10 @@
+/**
+ * Represents a simple example of Java code style conventions.
+ */
+public class CodeStyleExample {
+    
+    // A constant defined using SNAKE_CASE
+    public static final int MAX_ITEMS = 100;
+
+    // An instance variable using camelCase
+    private int itemCount;
